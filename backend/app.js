065b58c3b30bdb6fdbf6ccc6a9a568d102d3b41e
Anthony Bigotte <bigotte.anthony@gmail.com>
2021-09-22 10:25:50 +0200

    projet fini

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const path = require('path');
 
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content', 'Accept', 'Content-Type', 'Authorization']
+};
 
 const app = express();
 
@@ -15,18 +23,13 @@ mongoose.connect('mongodb+srv://piquante:secretpassword@cluster0.7nzki.mongodb.n
     })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+;
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); //authorise l'accés à la BDD à partir de n'importe quelle origine
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization,'); //dénifition des headers autorisés
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //définition des méthodes autorisées
-    if (req.method === 'OPTIONS') {
-        res.status(200);
-    }
-    next();
-});
+
 
 app.use(bodyParser.json());
+
+app.options('*', cors(corsOptions));
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('api/sauces', sauceRoutes);
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,5 +1,6 @@
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
+const { error } = require('console');
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
@@ -8,6 +9,10 @@ exports.createSauce = (req, res, next) => {
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
+    sauce.likes = 0;
+    sauce.dislikes = 0;
+    usersLike = [];
+    usersDislike = [];
     sauce.save()
         .then(() => res.status(201).json({ message: 'La sauce a bien été ajoutée !' }))
         .catch(error => res.status(400).json({ error }));
@@ -21,6 +26,43 @@ exports.modifySauce = (req, res, next) => {
         } : { ...req.body };
     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifyLikes = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            if (req.body.like === 1) {
+                if (sauce.usersLike.find(req.body.userId)) {
+                    return error({ error })
+                } else {
+                    sauce.usersLike.push(req.body.userId);
+                    sauce.likes += 1;
+                };
+            }
+            else if (req.body.like === -1) {
+                if (sauce.usersDislike.find(req.body.userId)) {
+                    return error({ error })
+                } else {
+                    sauce.usersDislike.push(req.body.userId);
+                    sauce.dislikes += 1;
+                };
+            }
+            else if (req.body.like === 0) {
+                if (sauce.usersLike.find(req.body.userId)) {
+                    const index = sauce.usersLike.indexOf(req.body.userId);
+                    if (index < -1) {
+                        sauce.usersLike.splice(index, 1);
+                    }
+                }
+                else if (sauce.usersDislike.find(req.body.userId)) {
+                    const index = sauce.usersDislike.indexOf(req.body.userId);
+                    if (index < -1) {
+                        sauce.usersDislike.splice(index, 1);
+                    }
+                }
+            }
+        })
         .catch(error => res.status(400).json({ error }));
 };
 
